d32a6cfdfdf0b42dd545c855bccfc6ccc7bb987e
13130110047 <358101181@qq.com>
2016-06-24 22:37:00 +0800

    Add files via upload

---
+++ Code/help_activity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.icourse.CollegeLifeManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class help_activity extends Activity {
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+		setTitle("°ïÖú");
+	}
+
+}
